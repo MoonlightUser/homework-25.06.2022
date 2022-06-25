f61027dff4e7ea843bd6199fac02a4be178bf4e5
Tymur <tymurmarenych@gmail.com>
2022-06-25 17:26:37 +0100

    created some const text and data base

---
+++ src/database.js
@@ -0,0 +1,1 @@
+let USERS_BASE = []
---
+++ src/texts.js
@@ -0,0 +1,3 @@
+const START_MENU_TEXT = "a) Registration\nb) Log In\nc) Check list of Users\nd) Change data of user\nq) Exit"
+const HELLO_TEXT = "Hello user, this is a program, which was created by me, Tymur Crutoi. Have fun"
+const WRONG_ANSWER_TEXT = `Please, fill in only "a" "b" "c" "d" "a"`
